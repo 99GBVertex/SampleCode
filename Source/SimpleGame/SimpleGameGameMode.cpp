6add5c60bcc0545f2b5cf8ac98612815d33ca6bc
99GBVertex <icecream3605@gmail.com>
2023-07-23 20:54:20 +0900

    delete start package Controller

--- Source/SimpleGame/SimpleGameGameMode.cpp
+++ Source/SimpleGame/SimpleGameGameMode.cpp
@@ -1,30 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SimpleGameGameMode.h"
-#include "SimpleGamePlayerController.h"
-#include "SimpleGameCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 
 ASimpleGameGameMode::ASimpleGameGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = ASimpleGamePlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
-
 	static ConstructorHelpers::FClassFinder<UUserWidget> WidgetClassFinder(TEXT("/Game/UI/UMG/Common/W_Master"));
 	if (WidgetClassFinder.Class != NULL) {
 		rootUI = WidgetClassFinder.Class;
